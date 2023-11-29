cc45d47e4bd513c890e7b65887559be6c4171062
PabloSan1997 <jos_pab_997@hotmali.com>
2023-11-28 22:54:59 -0600

    Se modifico el orden de las imagenes del usuario

--- server/services/usuario.services.ts
+++ server/services/usuario.services.ts
@@ -1,11 +1,11 @@
 import { AppDataSource } from "../database/config";
 import { Usuario } from "../database/models/Usuario";
-import {hash, compare} from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 import { jwt } from "../utilities/jsonwebtoken";
 import { Imagen } from "../database/models/Imagen";
 
 export class UsuarioServicio {
-    async agregarUsuario(nuevoUsuario:UsuarioCrear){
+    async agregarUsuario(nuevoUsuario: UsuarioCrear) {
         const repositorio = AppDataSource.getRepository(Usuario);
         const contra = await hash(nuevoUsuario.password, 8);
         nuevoUsuario.password = contra;
@@ -13,41 +13,51 @@ export class UsuarioServicio {
         await repositorio.manager.save(generarUsuario);
         return generarUsuario;
     }
-    async login(usuario:Login):Promise<LoginResponse>{
+    async login(usuario: Login): Promise<LoginResponse> {
         const repositorio = AppDataSource.getRepository(Usuario);
-        const ver = await repositorio.findOne({where:{user_name:usuario.user_name}});
-        if(!ver) throw new Error('Usuario o contraseña incorrectos');
+        const ver = await repositorio.findOne({ where: { user_name: usuario.user_name } });
+        if (!ver) throw new Error('Usuario o contraseña incorrectos');
         const checar = await compare(usuario.password, ver.password);
-        if(!checar) throw new Error('Usuario o contraseña incorrectos');
-        const respuesta:TokenResponse = {
-            nameUser:ver.id_usuario,
-            rol:ver.rol
+        if (!checar) throw new Error('Usuario o contraseña incorrectos');
+        const respuesta: TokenResponse = {
+            nameUser: ver.id_usuario,
+            rol: ver.rol
         }
         ver.password = await hash(usuario.password, 8);
-        await repositorio.update({id_usuario:ver.id_usuario}, ver);
+        await repositorio.update({ id_usuario: ver.id_usuario }, ver);
         const token = jwt.generarToken(respuesta);
-        return {permiso:true, token}
+        return { permiso: true, token }
     }
-    async leerUsuarioBPK(id_usuario:string, relacion=true){
+    async leerUsuarioBPK(id_usuario: string, relacion = true) {
         const repositorio = AppDataSource.getRepository(Usuario);
-        const usuario = await repositorio.findOne({where:{id_usuario}, relations:{imagenes:true}});
-        if(!usuario || !id_usuario) throw new Error('No se encontro usuario');
-        const {name, user_name, imagenes, url_perfil} = usuario;
-        const mostrar = imagenes.length!=0?imagenes.reverse():imagenes;
-        return {name, user_name,  url_perfil, imagenes:mostrar}
+        const reImagen = AppDataSource.getRepository(Imagen);
+        const usuario = await repositorio.findOne({ where: { id_usuario } });
+        console.log(usuario);
+        if (!usuario || !id_usuario) throw new Error('No se encontro usuario');
+        const { name, user_name, url_perfil } = usuario;
+        const imagenesUsuario = await reImagen.find({
+            where: { usuario },
+            relations: { usuario: true },
+            order: { creadedAt: 'DESC' }
+        });
+        const imagenes:ImagenesId[] = imagenesUsuario.map(p=>{
+            const {id_imagen, creadedAt, updatedAt, url_image, description, title}:ImagenesId = p;
+            return {id_imagen, creadedAt, updatedAt, url_image, description, title};
+        });
+        return { name, user_name, url_perfil, imagenes }
     }
-    async checarUsuario(id_usuario:string){
+    async checarUsuario(id_usuario: string) {
         const repositorio = AppDataSource.getRepository(Usuario);
-        const usuario = await repositorio.findOne({where:{id_usuario}});
-        if(!id_usuario) return false;
-        return !!usuario ;
+        const usuario = await repositorio.findOne({ where: { id_usuario } });
+        if (!id_usuario) return false;
+        return !!usuario;
     }
-    async deleteUsuario(id_usuario:string){
+    async deleteUsuario(id_usuario: string) {
         const repositorio = AppDataSource.getRepository(Usuario);
         const reImagenes = AppDataSource.getRepository(Imagen);
-        const usuario = await repositorio.findOne({where:{id_usuario}, relations:{imagenes:true}});
-        if(!!usuario && !!id_usuario){
-            await Promise.all(usuario.imagenes.map(async p =>{
+        const usuario = await repositorio.findOne({ where: { id_usuario }, relations: { imagenes: true } });
+        if (!!usuario && !!id_usuario) {
+            await Promise.all(usuario.imagenes.map(async p => {
                 await reImagenes.remove(p);
             }));
             await repositorio.remove(usuario);
